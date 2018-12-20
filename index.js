0ae15445ca0370ccad0c3b3d2c45624787e34949
Philip <pgilmor@gmail.com>
2018-12-19 22:49:33 -0600

    Updated Heroku postbuild

--- index.js
+++ index.js
@@ -16,5 +16,18 @@ app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
