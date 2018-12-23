96660fef1a342df099a4568576d3e3ea9c4f9026
Philip <pgilmor@gmail.com>
2018-12-23 10:20:52 -0600

    Remove Congress

--- congress/scripts/bills.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Refresh the bulk data collection.
-./run govinfo --bulkdata=BILLSTATUS
-
-# Turn into JSON and GovTrack-XML.
-./run bills --govtrack $@
--- congress/scripts/voteview.sh
+++
@@ -1,8 +0,0 @@
-for congress in {1..100}; do
-	./run voteview --congress=$congress --govtrack $@
-
-    # After the first run, no need to update legislator info.
-    export UPDATE_CONGRESS_LEGISLATORS=NO
-done
-./run voteview --govtrack --congress=101 --session=1989 --chamber=h $@
-
